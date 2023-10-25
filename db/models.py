4477666739d398862923d3dc002c33e61614982e
Anton <mainsample@yandex.ru>
2023-10-25 20:10:26 +0300

    edit model

--- db/models.py
+++ db/models.py
@@ -4,6 +4,8 @@ from sqlalchemy import (
     create_engine,
     select,
     func,
+    Table,
+    Column,
     ForeignKey,
     Integer,
     String,
@@ -24,12 +26,10 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(
-        Integer,
         nullable=False,
         unique=True, primary_key=True,
         autoincrement=True
     )
-
     create_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=True),
         nullable=False,
@@ -47,14 +47,23 @@ class Base(DeclarativeBase):
         return select(cls)
 
 
+category_vacancy = Table(
+    "category_vacancy",
+    Base.metadata,
+    Column("category_id", ForeignKey("Category.id"), primary_key=True),
+    Column("vacancy_id", ForeignKey("Vacancy.id"), primary_key=True),
+)
+
+
 class Category(Base):
     """Таблица категорий искомых вакансий"""
     __tablename__ = "Category"
     name: Mapped[PickleType] = mapped_column(PickleType, nullable=False)
-    no_search: Mapped[PickleType] = mapped_column(PickleType, nullable=True)
-    vacanciHH: Mapped[List["Vacancy"]] = relationship(
+    vacancy: Mapped[List["Vacancy"]] = relationship(
+        secondary=category_vacancy,
         back_populates="category",
-        cascade="all, delete-orphan"
+        cascade="all, delete-orphan",
+        single_parent=True
     )
 
 
@@ -64,33 +73,20 @@ class Vacancy(Base):
     hh_id: Mapped[int] = mapped_column(nullable=False)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     employer: Mapped[str] = mapped_column(String(255), nullable=False)
-    published_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True))
+    published_at: Mapped[Date] = mapped_column(Date)
     experience: Mapped[str] = mapped_column(String(50), nullable=True)
     metro_station: Mapped[str] = mapped_column(String(255), nullable=True)
     salary: Mapped[str] = mapped_column(String(255), nullable=True)
-    url: Mapped[str] = mapped_column(nullable=False)
-    category_id: Mapped[int] = mapped_column(ForeignKey("Category.id"))
-    category: Mapped["Category"] = relationship(back_populates="vacanciHH")
+    url: Mapped[str] = mapped_column(String(255), nullable=False)
+    view: Mapped[bool] = mapped_column(Boolean, default=False)
+    category: Mapped[List[Category]] = relationship(
+        secondary=category_vacancy,
+        back_populates="vacancy",
+    )
 
     def __repr__(self) -> str:
-        return f"name = {self.name}, employer = {self.employer}, url = {self.url}"
-
-
-if __name__ == "__main__":
-    engine = create_engine("sqlite:///./vacancy.db", echo=True) 
-    Base.metadata.create_all(engine)
-
-    with Session(engine) as session:
-        jun = Category(name=['junior'])
-        jun_py = Category(name=['junior', 'python'])
-        jun_dev = Category(name=['junior', 'devops'])
-        jun_sre = Category(name=['junior', 'sre'])
-        bac_py = Category(name=['backend', 'python'])
-        dev = Category(name=['devops'])
-        sre = Category(name=['sre'])
-        session.add_all([jun, sre, jun_py, jun_dev, jun_sre, bac_py, dev, sre])
-        session.commit()
-        command = select(Category)
-        cat = session.scalars(command)
-        for i in cat:
-            print(i.name, ' '.join(i.name))
+        return f"name = {0}, employer = {1}, url = {2}".format(
+            self.name,
+            self.employer,
+            self.url
+        )
